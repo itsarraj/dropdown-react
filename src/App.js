1fa391166ea5ea4e21e7256c29c3486bc171c90e
Animesh Raj <animesh080808@gmail.com>
2023-05-15 17:53:31 +0530

    done
    Signed-off-by: Animesh Raj <animesh080808@gmail.com>

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
+import { useEffect, useState } from 'react';
 import { Head, Options } from './component/index';
 
 import styles from './styles/app.module.css';
 
 function App() {
+    const options = ['Yes', 'Probably Not'];
     return (
         <div className={styles.app}>
             <Head />
-            <Options />
+            <Options options={options} />
         </div>
     );
 }
--- src/component/Options.js
+++ src/component/Options.js
@@ -1,26 +1,47 @@
+import { useEffect, useState } from 'react';
 import styles from '../styles/options.module.css';
 
-const Options = () => {
+const Options = ({ options }) => {
+    const [displayOptions, setDisplayOptions] = useState(false);
+
+    const handleMouseEnter = () => {
+        setDisplayOptions(true);
+    };
+    const handleMouseLeave = () => {
+        setDisplayOptions(false);
+    };
+    const handleOptionClick = (option) => {
+        console.log('Selected option:', option);
+        setDisplayOptions(false);
+    };
     return (
         <div className={styles.options}>
-            <select name="Select" className={styles.select}>
-                <option
-                    hidden
-                    value="Select"
-                    className={styles.selectoptionselect}
-                >
-                    Select
-                </option>
-                <option value="Yes" className={styles.selectoptionyes}>
-                    Yes
-                </option>
-                <option
-                    value="Probably Not"
-                    className={styles.selectoptionprobablynot}
-                >
-                    Probably Not
-                </option>
-            </select>
+            <div
+                className={styles.button}
+                onMouseEnter={handleMouseEnter}
+                onMouseLeave={handleMouseLeave}
+            >
+                <button className={styles.button}>Select</button>
+                <div className={styles.dropdown}>
+                    {displayOptions && (
+                        <>
+                            <ul className={styles.dropdownMenu}>
+                                {options.map((option, index) => (
+                                    <li
+                                        key={index}
+                                        className={styles.dropdownItem}
+                                        onClick={() =>
+                                            handleOptionClick(option)
+                                        }
+                                    >
+                                        {option}
+                                    </li>
+                                ))}
+                            </ul>
+                        </>
+                    )}
+                </div>
+            </div>
         </div>
     );
 };
